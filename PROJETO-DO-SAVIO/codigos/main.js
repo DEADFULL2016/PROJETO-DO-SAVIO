ddbc7f38a813a009ac8d400608ce51de6645d0b1
DEADFULL2016 <aalvesoliveira0401@gmail.com>
2024-06-10 12:47:38 -0300

    td

--- PROJETO-DO-SAVIO/codigos/main.js
+++ PROJETO-DO-SAVIO/codigos/main.js
@@ -1,32 +1,15 @@
-const input = document.getElementById('nome')
-const button = document.getElementById('entrar')
+document.addEventListener('DOMContentLoaded', () => {
+    const input = document.getElementById('nome');
+    const button = document.getElementById('entrar');
 
-button.addEventListener('click',(event)=>{
-   event.preventDefault()
-   if (input.value.length < 3){
-       alert('nome muito curto!')
-    if (input.value.length == 0){
-        alert('digite seu nome para continuar!')
- }
-}
-
-})
-entrar.addEventListener('click',(event)=>{
-    event.preventDefault()
- 
- })
-
- function entrar(){
-
-    if (input.value.length < 3){
-        alert('nome muito curto!')
-     if (input.value.length == 0){
-         alert('digite seu nome para continuar!')
-  }     
-  else{
-    window.location.href = './menu inicial.html'
-
-}
-}
-
- }
+    button.addEventListener('click', (event) => {
+        event.preventDefault();
+        if (input.value.length == 0) {
+            alert('Digite seu nome para continuar!');
+        } else if (input.value.length < 3) {
+            alert('Nome muito curto!');
+        } else {
+            window.location.href = 'menu inicial.html'; 
+        }
+    });
+});
