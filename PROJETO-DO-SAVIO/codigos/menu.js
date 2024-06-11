40a641edc8dfb418db2d1e5f5e8f55628c949918
DEADFULL2016 <aalvesoliveira0401@gmail.com>
2024-06-11 16:21:28 -0300

    adicionando preco

--- PROJETO-DO-SAVIO/codigos/menu.js
+++ PROJETO-DO-SAVIO/codigos/menu.js
@@ -1,4 +1,13 @@
-document.addEventListener('DOMContentLoaded', () => {
-    constbutton = document.getElementById('carrinho');
-    constbutton = document.getElementById('detalhes');
+const button = document.querySelectorAll('.carrinho');
+let precoTotal = 0
+const precoTotalDiv = document.querySelector('.precoTotal');
+
+button.forEach((btn) => {
+    btn.addEventListener('click', (event) => {
+        const item = event.target.closest(".item");
+        const preco = item.querySelector('.preco').innerHTML;
+
+        precoTotal += Number(preco.slice(2));
+        precoTotalDiv.innerHTML = `Total: R$ ${precoTotal.toFixed(2)}`
+    });
 })
